0ed672d746e9cc7b49581d21c25bf2f4e9fdc477
RMB-NT\vjo <vernon.joyce@rmb.co.za>
2017-10-04 21:40:52 +0200

    Added employee ID

--- index.js
+++ index.js
@@ -60,13 +60,13 @@ app.post('/webhook', function (req, res, next) {
     })
   }
 
-  if (action === 'emplyee_number') {
+  if (action === 'employee_number') {
     let name = req.body.result.parameters.first_name;
     let surname = req.body.result.parameters.surname;
-    let api = `http://52.179.15.57:8080/locate/employee/${name}/${surname}`
+    let api = `http://52.179.15.57:8080/get/employee/${name}/${surname}`
     request(api, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-          let message = JSON.parse(body).length !== 0 ? `${name} ${surname} sits in ${JSON.parse(body)[0].location}` : 'This user was not found';
+          let message = JSON.parse(body).length !== 0 ? `Your employee number is ${JSON.parse(body)[0].employeeId}` : 'This user was not found';
           res.send({
             speech: message,
             displayText: message,
