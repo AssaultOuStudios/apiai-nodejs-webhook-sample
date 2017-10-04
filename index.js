41aef8b9737bf7007e73b0798fae762e8511f369
RMB-NT\vjo <vernon.joyce@rmb.co.za>
2017-10-04 18:32:28 +0200

    Added name webhook

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
@@ -10,19 +11,53 @@ const AUTH_TOKEN = '16a0d80c05d7403eb97811641f77723b'
 app.get('/', function (req, res) {
   res.send('Use the /webhook endpoint.')
 })
-app.get('/webhook', function (req, res) {
-  res.send('You must POST your request')
-})
 
-app.post('/webhook', function (req, res) {
+app.post('/webhook', function (req, res, next) {
   //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    return res.json({
-        speech: req.body.result.parameters["given-name"] + req.body.result.parameters.employee_surname + " sits in 2nd Floor 2 Merchant Place",
-        displayText: req.body.result.parameters["given-name"] + req.body.result.parameters.employee_surname + " sits in 2nd Floor 2 Merchant Place",
-        source: 'webhook-echo-sample'
-    });
+    let name = req.body.result.parameters.first_name;
+    let surname = req.body.result.parameters.surname;
+    let api = `http://52.179.15.57:8080/location/${name}/${surname}`
+    request(api, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          res.send({
+            speech: `${name} ${surname} sits in ${JSON.parse(body)[0].location}`,
+            displayText: `${name} ${surname} sits in ${JSON.parse(body)[0].location}`,
+            source: 'location-webhook'
+          });
+      }
+    })
 })
 
 app.listen(app.get('port'), function () {
   console.log('* Webhook service is listening on port:' + app.get('port'))
 })
+
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const app = express()
+// app.use(bodyParser.json())
+// app.set('port', (process.env.PORT || 5000))
+//
+// const REQUIRE_AUTH = true
+// const AUTH_TOKEN = '16a0d80c05d7403eb97811641f77723b'
+//
+// app.get('/', function (req, res) {
+//   res.send('Use the /webhook endpoint.')
+// })
+// app.get('/webhook', function (req, res) {
+//   res.send('You must POST your request')
+// })
+//
+// app.post('/webhook', function (req, res) {
+//   //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+//     return res.json({
+//         speech: req.body.result.parameters["given-name"] + req.body.result.parameters.employee_surname + " sits in 2nd Floor 2 Merchant Place",
+//         displayText: req.body.result.parameters["given-name"] + req.body.result.parameters.employee_surname + " sits in 2nd Floor 2 Merchant Place",
+//         source: 'webhook-echo-sample'
+//     });
+// })
+//
+// app.listen(app.get('port'), function () {
+//   console.log('* Webhook service is listening on port:' + app.get('port'))
+// })
